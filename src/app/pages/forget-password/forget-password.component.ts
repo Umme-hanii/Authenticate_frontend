311b69b9f4cf7994ebffeb928d9d42c976fcd372
Umme-hanii <ummehanimohammed9@gmail.com>
2023-10-07 09:41:42 +0530

    Configure Routing.

--- src/app/pages/forget-password/forget-password.component.ts
+++ src/app/pages/forget-password/forget-password.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './forget-password.component.html',
-  styleUrls: ['./forget-password.component.scss']
+  styleUrls: ['./forget-password.component.scss'],
 })
-export class ForgetPasswordComponent {
-
-}
+export default class ForgetPasswordComponent {}
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
-
-}
+export default class RegisterComponent {}
